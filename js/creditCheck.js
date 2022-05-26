36bc33b98f1b09b29b4b0602f939d1d372e495ef
colour-me-bibi <bennyforeman1@gmail.com>
2022-05-26 13:37:43 -0700

    Pair programming

--- js/creditCheck.js
+++ js/creditCheck.js
@@ -5,7 +5,33 @@
  * @param {String} string The credit card number to validate
  */
 const creditCheck = (string) => {
+  const reversed = Array.from(string).reverse().map(digit => parseInt(digit));
 
+  const result = [];
+
+  for (let i = 0; i < reversed.length ; i++) {
+    if (i % 2 == 1) {
+      const newDigit = reversed[i] * 2;
+
+      if (newDigit < 10) {
+        result.push(newDigit);
+      } else {
+        let digitSum = 0;
+
+        for (const digit of Array.from(newDigit.toString()).map(digit => parseInt(digit))) {
+          digitSum += digit;
+        }
+
+        result.push(digitSum)
+      }
+    } else {
+      result.push(reversed[i])
+    }
+  }
+
+  const resultSum = result.reduce((acc, curr) => acc + curr, 0);
+
+  return (resultSum % 10 === 0) ? "The number is valid!" : "The number is invalid!"; 
 };
 
 module.exports = { creditCheck };
--- python/credit_check.py
+++ python/credit_check.py
@@ -17,3 +17,34 @@ def credit_check(string):
     Returns:
         bool: True if the given string is a valid credit card number
     """
+
+    digits = [int(digit) for digit in string]
+
+    # for i, el in enumerate(list(reversed(digits))):
+    #     pass
+
+    # for i in range(len(digits) - 2, -1, -2):
+    #     digits[i]
+
+    new_digits = list()
+
+    for i, digit in enumerate(digits[::-1]):  # we're going backwards through the list
+        if i % 2 == 1:
+            new_digit = digit * 2
+
+            if new_digit < 10:
+                new_digits.append(new_digit)
+            else:
+                new_digits.append(sum(int(digit) for digit in str(new_digit)))
+        else:
+            new_digits.append(digit)
+
+    return "The number is valid!" if sum(new_digits) % 10 == 0 else "The number is invalid!"
+
+
+result = credit_check('5541808923795240')  # "The number is valid!"
+print(result)
+result = credit_check("4024007136512380")  # "The number is valid!"
+print(result)
+result = credit_check("6011797668867828")  # "The number is valid!"
+print(result)
